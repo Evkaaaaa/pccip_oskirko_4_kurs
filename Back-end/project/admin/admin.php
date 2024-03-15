d306b64fab333ee51a885656b9a2363dbf98ccb0
Evkaaaaa <119289626+Evkaaaaa@users.noreply.github.com>
2024-03-16 00:36:31 +0300

    Add files via upload

---
+++ Back-end/project/admin/admin.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Админ-панель</title>
+    <link rel="stylesheet" href="/assets/css/admin.css">
+</head>
+
+<body>
+    <?php require "../users/blocks/userheader.php" ?>
+    <h1>Добро пожаловать в админ панель!</h1>
+    
+
+</body>
+
+</html>
---
+++ Back-end/project/admin/adminheader.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/assets/css/userheader.css">
+</head>
+<body>
+    <div class="container">
+        <ul>
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">Уроки</a></li>
+            <li><a href="/profile.php">Профиль пользователя</a></li>
+            <li><a href="../include/logoutamin.php">Выход</a></li>
+        </ul>
+    </div>
+</body>
+</html>
